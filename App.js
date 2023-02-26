b0548d8bf2516861cc89e361928901d2eeb0329e
Sean Keefe <sean.c.keefe13@gmail.com>
2023-02-25 22:10:59 -0500

    changed styling added correct path for footer

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Info from './Components/Info'
 import About from './Components/About'
 import Interests from './Components/Interests'
-import Footer from './Components/Interests'
+import Footer from './Components/Footer'
 
 export default function App(){
     return (
